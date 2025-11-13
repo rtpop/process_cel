8858c483d8a00618e6a1fe53a634f9e6c68a5182
rtpop <romanatab@hotmail.com>
2025-11-13 08:32:19 +0100

    added container

---
+++ containers/requirements.R
@@ -0,0 +1,55 @@
+##-----------------------------##
+## List of R packages required ##
+##-----------------------------##
+
+## CRAN packages:
+required_packages_cran <- c(
+    "BiocManager",    # To install Bioconductor packages
+    "dplyr",          # For data manipulation
+    "data.table",     # For data manipulation with data.table
+    "optparse",       # For reading command-line arguments
+    "tidyr",          # For tidying data
+    "ggplot2",        # For creating plots
+    "ggrepel",        # For adding labels to ggplot2 plots
+    "ggpubr",         # For ggplot2 publication-ready plots
+    "magrittr",       # For pipe operator %<>%
+    "pheatmap",       # For drawing heatmaps
+    "RColorBrewer",   # For color palettes for heatmaps
+    "remotes",        # For installing local or GitHub packages
+    "sessioninfo",    # For session information
+    "stringr"         # For string manipulation
+    
+    )
+
+install.packages(
+    required_packages_cran,
+    dependencies = TRUE, verbose = TRUE)
+
+## Bioconductor packages:
+required_packages_bioconductor <- c(
+    "affy",            # For processing Affymetrix microarray data
+    "biomaRt",          # For accessing BioMart databases
+    "ComBat",           # For batch effect correction
+    "limma",            # For limma analysis
+    "fgsea",            # For gene set enrichment analysis
+    "org.Hs.eg.db",     # For gene annotations
+    "pd.hta.2.0",       # For processing Affymetrix microarray data
+    "hta20cdf",         # For processing Affymetrix microarray data
+    "oligo",            # For processing oligonucleotide arrays
+    "pd.huex.1.0.st.v1" # For processing Affymetrix microarray data
+)
+
+BiocManager::install(
+    required_packages_bioconductor)
+
+## GitHub packages:
+remotes::install_github("kuijjerlab/MARMOT")  # For multi-omics data integration
+
+## Install any local tarballs placed into /opt
+local_tarballs <- list.files("/opt", pattern="\\.tar\\.gz$", full.names=TRUE)
+if (length(local_tarballs) > 0) {
+  if (!requireNamespace("remotes", quietly=TRUE)) {
+    install.packages("remotes", repos="https://cloud.r-project.org")
+  }
+  remotes::install_local(local_tarballs, dependencies = TRUE)
+}

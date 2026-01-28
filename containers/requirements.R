e711e32a1e28fb59c8fbff246fa04ec32a539b5c
Romana Pop <romanatab@homail.com>
2026-01-28 11:07:13 +0100

    switched to using my base_r container as the starting point

--- containers/requirements.R
+++ containers/requirements.R
@@ -5,22 +5,7 @@
 options(repos = c(CRAN = "https://cran.r-project.org"))
 
 ## CRAN packages:
-required_packages_cran <- c(
-    "BiocManager",    # To install Bioconductor packages
-    "dplyr",          # For data manipulation
-    "data.table",     # For data manipulation with data.table
-    "optparse",       # For reading command-line arguments
-    "tidyr",          # For tidying data
-    "ggplot2",        # For creating plots
-    "ggrepel",        # For adding labels to ggplot2 plots
-    "ggpubr",         # For ggplot2 publication-ready plots
-    "magrittr",       # For pipe operator %<>%
-    "pheatmap",       # For drawing heatmaps
-    "RColorBrewer",   # For color palettes for heatmaps
-    "remotes",        # For installing local or GitHub packages
-    "sessioninfo",    # For session information
-    "stringr"         # For string manipulation
-    )
+required_packages_cran <- c()
 
 install.packages(
     required_packages_cran,
@@ -29,12 +14,8 @@ install.packages(
 ## Bioconductor packages:
 required_packages_bioconductor <- c(
     "affy",             # For processing Affymetrix microarray data
-    "biomaRt",          # For accessing BioMart databases
-    "sva",              # For batch effect correction
-    "org.Hs.eg.db",     # For gene annotations
     "pd.hta.2.0",       # For processing Affymetrix microarray data
     "hta20cdf",         # For processing Affymetrix microarray data
-    "pcaMethods"       # For PCA analysis
 )
 
 BiocManager::install(
